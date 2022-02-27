a4a096eadc6c8b72a948fd83e10601d1fa6df486
bailingshiva <839665036@qq.com>
2022-02-27 15:11:54 +0800

    daimatijiao

---
+++ src/views/editor.vue
@@ -0,0 +1,45 @@
+<template>
+ <mavon-editor
+   v-model="data.content"
+   ref="md"
+   @imgAdd="$imgAdd"
+   @change="change"
+   style="min-height: 600px"
+ />
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, reactive, ref } from 'vue';
+import { mavonEditor } from "mavon-editor";
+import "mavon-editor/dist/css/index.css";
+
+export default defineComponent({
+  components: {
+    mavonEditor
+  },
+ setup() {
+  const data = reactive({ content: '' });
+  return {data}
+ },
+  methods: {
+    // 将图片上传到服务器，返回地址替换到md中
+    $imgAdd(pos, $file) {
+      // var formdata = new FormData();
+      // formdata.append("file", $file);
+      // //将下面上传接口替换为你自己的服务器接口
+      // axios({
+      //   url: "/common/upload",
+      //   method: "post",
+      //   data: formdata,
+      //   headers: { "Content-Type": "multipart/form-data" }
+      // }).then(url => {
+      //   this.$refs.md.$img2Url(pos, url);
+      // });
+    },
+    change(value, render) {
+      // // render 为 markdown 解析后的结果
+      // this.html = render;
+    }
+    }
+})
+</script>
---
+++ src/views/editor1.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <Editor :value="value"/>
+    <Viewer class="viewer" :tabindex="2" :value="value"
+    ></Viewer>
+</div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, reactive, ref } from 'vue';
+
+import 'bytemd/dist/index.min.css'
+import { Editor,Viewer } from '@bytemd/vue'
+import gfm from '@bytemd/plugin-gfm'
+import highlight from "@bytemd/plugin-highlight-ssr";
+
+const pluginsArr = [
+  //将所有的扩展功能放入插件数组中，然后就可以生效了    
+  gfm(),
+  highlight(),
+]
+
+export default defineComponent({
+  name: 'User',
+  components: { Editor,Viewer },
+  setup() {
+    const plugins = pluginsArr;
+    const value = computed(() => '');
+    console.log(plugins, value.value)
+    return { plugins, value};
+  }
+});
+</script>
